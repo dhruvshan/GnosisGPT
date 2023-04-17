7604727210b28325611c3ed920bc2ff16093bce5
Dhruv Shanbhag <dhruvshan@gmail.com>
2023-04-17 18:01:59 +0800

    Updated running script

--- app.py
+++ app.py
@@ -63,5 +63,6 @@ def ask_gpt():
 
 # function to authorize or download latest credentials 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    # from waitress import serve
+    # serve(app, host="0.0.0.0", port=8080)
+    app.run()
